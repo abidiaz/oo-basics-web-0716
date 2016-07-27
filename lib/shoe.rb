ac3c72ac2c17ed73bf7bf4942794f24daf112502
Abigail Diaz <github email address>
2016-07-27 14:00:47 -0400

    OO Basics lab completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
+class Shoe
+attr_accessor(:color, :size, :material, :condition)
+attr_reader(:brand)
+
+def initialize(brand)
+  @brand = brand
+  # @color = color
+  # @size = size
+  # @material = material
+  # @condition = condition
+end
+
+def cobble
+  self.condition = "new"
+  puts "Your shoe is as good as new!"
+end
+
+end
+
 # Make your shoe class here!
